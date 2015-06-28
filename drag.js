2fd1f644506cf2725dad3dac12c7ca8357738256
nijiulin <13550017584@163.com>
2015-06-28 20:35:15 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -6,6 +6,8 @@ function drag(id){
     var disX = 0;
     var disY = 0;
     obj.onmousedown = function(){
+        disX = ev.pageX - obj.offsetLeft;
+        disY = ev.pageY - obj.offsetTop;
     }
 }
 
